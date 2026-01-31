8dff26005cfbc13900f9298c22d44252199bae27
mehmetsn101-lab <mehmetsn101@gmail.com>
2026-02-01 01:02:51 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+
+const CACHE = "stokbulucu-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-180.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).catch(() => caches.match("./index.html")))
+  );
+});
